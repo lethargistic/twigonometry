ce79c0c4ef190af33f0c5085ea6d306344f642f8
Maksiks <maksiksking@gmail.com>
2025-11-22 19:45:14 +0200

    pain: setup gradle w kotlin and publishing help this took two days

--- example/common/src/main/java/com/example/examplemod/CommonClass.java
+++ example/common/src/main/java/com/example/examplemod/CommonClass.java
@@ -1,6 +1,7 @@
 package com.example.examplemod;
 
 import com.example.examplemod.platform.Services;
+import dev.maksiks.twigonometry.api.LeafPlacerContext;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Items;
 
@@ -17,6 +18,7 @@ public class CommonClass {
 
         Constants.LOG.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
         Constants.LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
+        Constants.LOG.info("Diesel wiesel {}", LeafPlacerContext.getEmpty());
 
         // It is common for all supported loaders to provide a similar feature that can not be used directly in the
         // common code. A popular way to get around this is using Java's built-in service loader feature to create
---
+++ example/common/src/main/java/com/example/examplemod/tree/ModTreeGrowers.java
@@ -0,0 +1,4 @@
+package com.example.examplemod.tree;
+
+public class ModTreeGrowers {
+}
